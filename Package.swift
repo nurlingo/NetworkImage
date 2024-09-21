708911ced7f14f3290ec8c9b39a876b4b7dbb4a9
Nursultan Askarbekuly <nursultan@outlook.com>
2024-09-21 23:48:46 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,10 +6,7 @@ import PackageDescription
 let package = Package(
   name: "NetworkImage",
   platforms: [
-    .macOS(.v11),
-    .iOS(.v11),
-    .tvOS(.v14),
-    .watchOS(.v7),
+    .iOS(.v11)
   ],
   products: [
     .library(name: "NetworkImage", targets: ["NetworkImage"])
